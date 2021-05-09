0953bd66cdc1afa9c1c4b393a91066cac2072410
lsongzhi <lsongzhi@163.com>
2021-05-09 22:19:48 +0800

    feat

--- src/flags.rs
+++ src/flags.rs
@@ -39,3 +39,43 @@ impl OpenFlags {
         self.contains(OpenFlags::APPEND)
     }
 }
+
+bitflags! {
+    pub struct AtFlags: usize {
+        const EMPTY_PATH = 0x1000;
+        const SYMLINK_NOFOLLOW = 0x100;
+    }
+}
+
+bitflags! {
+    pub struct CloneFlags: usize {
+        const CSIGNAL =         0x000000ff;
+        const VM =              0x00000100;
+        const FS =              0x00000200;
+        const FILES =           0x00000400;
+        const SIGHAND =         0x00000800;
+        const PTRACE =          0x00002000;
+        const VFORK =           0x00004000;
+        const PARENT =          0x00008000;
+        const THREAD =          0x00010000;
+        const NEWNS	 =          0x00020000;
+        const SYSVSEM =         0x00040000;
+        const SETTLS =          0x00080000;
+        const PARENT_SETTID =   0x00100000;
+        const CHILD_CLEARTID =  0x00200000;
+        const DETACHED =        0x00400000;
+        const UNTRACED =        0x00800000;
+        const CHILD_SETTID =    0x01000000;
+        const NEWCGROUP =       0x02000000;
+        const NEWUTS =          0x04000000;
+        const NEWIPC =          0x08000000;
+        const NEWUSER =         0x10000000;
+        const NEWPID =          0x20000000;
+        const NEWNET =          0x40000000;
+        const IO =              0x80000000;
+    }
+}
+
+/// Pathname is interpreted relative to the current working directory(CWD)
+pub const AT_FDCWD: usize = -100isize as usize;
+
